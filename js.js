00a19fab4d6e310607360da2c6a73ece9acf3e09
JessicaSid <62107447+JessicaSid@users.noreply.github.com>
2020-03-28 12:02:31 +0100

    testing

--- js.js
+++ js.js
@@ -1,7 +1,28 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navig');
-    const navLinks = document.querySelectorAll('.navig li');
+    const nav = document.querySelector('.nav-links');
+
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+    });
+
+    navLinks.forEach((links, index) => {
+        links.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+
+    });
+
+}
+
+navSlide();
+
+
+
+/*const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
 
@@ -12,7 +33,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = 'navigfade 0.5s ease forwards ${index / 7 + 0}'
+                link.style.animation = 'nav-linksfade 0.5s ease forwards ${index / 7 + 0}'
             }
         });
 
@@ -23,4 +44,4 @@ const navSlide = () => {
 }
 
 
-navSlide();
+navSlide();*/
