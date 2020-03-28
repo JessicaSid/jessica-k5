ec5583572c385596ad27dc2ed33654ed7538728b
JessicaSid <62107447+JessicaSid@users.noreply.github.com>
2020-03-28 23:22:21 +0100

    Removing old js- files

--- portfolio.js
+++ portfolio.js
@@ -1,25 +1,27 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navig');
-    const navig = document.querySelectorAll('.navig li');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
 
-        navig.forEach((links, index) => {
+        navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = 'navigFade 0.5 ease forwards ${index / 7 + 1.5}s'
+                link.getElementsByClassName.animation = 'navLinkFade 0.5s ease forwards $ {index / 7 + 1.5}s';
             }
         });
 
         burger.classList.toggle('toggle');
+
     });
 
 
-}
 
+}
 
 navSlide();
